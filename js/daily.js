c2b925311a536ebc7b30ec92a4e473dfc1b871f4
Joey Jaikaran <j.r.d.jaikaran@gmail.com>
2026-05-02 18:15:47 -0400

    add daily.js - deterministic daily pokemon selection

---
+++ js/daily.js
@@ -0,0 +1,28 @@
+import { state, initGame } from "./state.js"
+
+function getDailyPokemon() {
+    const start = new Date("2025-01-01");
+    const today = new Date();
+    // Ensure that player gets the same pokemon regardless of the time of day they play
+    today.setHours(0, 0, 0, 0);
+
+    // Convert millisecond difference to number of days
+    const daysSinceStart = Math.floor((today - start) /  (1000 * 60 * 60 * 24));
+    // Use modulo to keep index within bounds of dailyPokemon array
+    const index = daysSinceStart % state.dailyPokemon.length;
+
+    return state.dailyPokemon[index];
+}
+
+function hasPlayedToday() {
+    const today = new Date().toISOString().split("T")[0];
+    return state.lastPlayed === today;
+}
+
+function startDailyGame() {
+    initGame();
+    state.gameMode = "daily";
+    state.answer = getDailyPokemon();
+}
+
+export { startDailyGame, hasPlayedToday };
